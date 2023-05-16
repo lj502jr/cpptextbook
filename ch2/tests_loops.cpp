05d9d9b9058cd45a86473911e14f48b1c643e1b6
John Barnes VI <lj502jr@gmail.com>
2023-05-15 23:32:14 -0500

    update ch2.2.4

--- ch2/tests_loops.cpp
+++ ch2/tests_loops.cpp
@@ -3,21 +3,24 @@ using namespace std;
 
 bool accept()
 {
-    cout << "Do you want to proceed (y or n)?\n";   // write question
-
-    char answer = 0;
-    cin >> answer;                                  // read answer
-    switch(answer)
+    int tries = 1;
+    while (tries<4)
     {
-        case 'y':
-            cout << "Accepted.\n";
-            return true;
-        case 'n':
-            cout << "Not accepted.\n";
-            return false;
-        default:
-            cout << "I'll take that for a no.\n";
-            return false;
+        cout << "Do you want to proceed (y or n)?\n";   // write question
+        char answer = 0;
+        cin >> answer;                                  // read answer
+        switch(answer)
+        {
+            case 'y':
+                cout << "Accepted.\n";
+                return true;
+            case 'n':
+                cout << "Not accepted.\n";
+                return false;
+            default:
+                cout << "I'll take that for a no.\n";
+                ++tries;   // increment
+        }
     }
 }
 
